70a3b9fb6ac545c067d05fab20371a884bd8e668
Aandi1989 <navaliny1989@gmail.com>
2023-11-18 18:46:08 +0300

    ButtonRippleEffect First Variant

---
+++ ButtonRippleEffect/script.js
@@ -0,0 +1,36 @@
+"use strict"
+
+document.addEventListener("click", function(e){
+    const targetItem = e.target;
+    if(targetItem.closest('[data-ripple]')){
+        // Константы
+        const button = targetItem.closest('[data-ripple]');
+        const ripple = document.createElement('span');
+        const diameter = Math.max(button.clientWidth, button.clientHeight);
+        const radius = diameter / 2;
+
+        // Формируем элемент
+        ripple.style.width = ripple.style.height = `${diameter}px`;
+        ripple.style.left = `${e.pageX - (button.getBoundingClientRect().left + scrollX) - radius}px`;
+        ripple.style.top = `${e.pageY - (button.getBoundingClientRect().top + scrollY) - radius}px`;
+        ripple.classList.add('ripple');
+
+        // Добавление элемента
+        button.appendChild(ripple);
+
+        // Получение времени действия анимации
+        const timeOut = getAnimationDuration();
+
+        // Удаление элемента
+        setTimeout(() => {
+            ripple ? ripple.remove() : null;
+        }, timeOut);
+
+        // Функция получения времени действия анимации
+        function getAnimationDuration() {
+            const Duration = window.getComputedStyle(ripple).animationDuration;
+            return Duration.includes('ms') ? 
+                Duration.replace("ms", '') : Duration.replace("s", '') * 1000;
+        }
+    }
+})
